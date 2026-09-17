f3f5dc8f9f6a1341c0acef09411c9ee4ac881ce1
Duy Linh <linhtdts02636@gmail.com>
2026-09-17 12:12:29 +0700

    done lab1

---
+++ LAB1/demo.js
@@ -0,0 +1,23 @@
+console.log("bài 1")
+const studentId = "TS02636"
+const studentName = "tran duy linh"
+const studentDOB = "20-6-2009"
+const studentPOB = "HCM city"
+const major = "software development"
+
+const arr = [studentId, studentName, studentDOB, studentPOB, major]
+
+console.log(arr)
+const notDefined = undefined
+console.log(typeof notDefined === "undefined")
+
+let nothing = null
+console.log(typeof nothing === "object")
+
+console.log("10" + 5)
+console.log("10" - 5)
+console.log(Number("123"))
+console.log(String(123))
+console.log(Boolean(""))
+console.log(+"10")
+
