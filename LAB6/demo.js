959dd2f478f8e3c4d135ff36745570d3745ae8f5
linhtranduy <linhtdts02636@gmail.com>
2026-09-22 14:09:00 +0700

    halfway done lab 6

---
+++ LAB6/demo.js
@@ -0,0 +1,16 @@
+function studentWindow(){
+const width = 600
+const height = 400
+
+const left = (screen.width - width)/2;
+const top = (screen.height - width)/2;
+
+
+const studentWindow = window.open(
+    "",
+    "student info",
+    `width=${width}, height=${height}, top=${top}, left =${left} `
+)
+
+const element = `.d`
+}
